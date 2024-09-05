501c6a31e79753aad8bdeef6e9450f924a8c6f19
THOMAS AUGOT <thomas.augot@hotmail.fr>
2024-09-05 18:43:31 +0200

    first api call ok

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/api/home", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
