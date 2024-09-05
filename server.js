9f5b8a06cb0fdb30108bb155c7aac477b2bbd050
THOMAS AUGOT <thomas.augot@hotmail.fr>
2024-09-05 18:44:50 +0200

    first api call ok - server up and running

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 8080;
+
+app.use(cors());
 
 app.get("/api/home", (req, res) => {
   res.json({ message: "Hello World" });
